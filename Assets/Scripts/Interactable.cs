333a174076f6e9d6d8be86136d648b21ee5437db
Jose Striediner <jstriedinger2090@gmail.com>
2023-06-26 19:47:45 -0700

    Outline for interactable items

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -3,29 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+[RequireComponent(typeof(Outline))]
 public abstract class Interactable : MonoBehaviour
 {
     [SerializeField] private bool ShowPopup;
     [SerializeField] private Transform PopupText;
+    [SerializeField] private bool ShowOutline = true;
 
     private Vector3 startSize;
+    private Outline myOutline;
     [SerializeField, ReadOnly] private bool isInRange;
 
     protected virtual void Awake()
     {
-        PopupText = transform.GetChild(0).GetChild(0);
-        startSize = PopupText.localScale;
-        PopupText.localScale = Vector3.zero;
+        if(ShowPopup)
+        {
+            PopupText = transform.GetChild(0).GetChild(0);
+            startSize = PopupText.localScale;
+            PopupText.localScale = Vector3.zero;
+
+        }
+        myOutline = GetComponent<Outline>();
+        myOutline.enabled = false;
     }
 
     // Update is called once per frame
     protected virtual void Update()
     {
-        if (isInRange && ShowPopup)
+        if (isInRange)
         {
-            PopupText.localScale = Vector3.Lerp(PopupText.localScale, startSize, 0.05f);
+            if(ShowPopup)
+                PopupText.localScale = Vector3.Lerp(PopupText.localScale, startSize, 0.05f);
+            if (ShowOutline)
+                myOutline.enabled = true;
+
         }
-        else if (!isInRange && ShowPopup) { PopupText.localScale = Vector3.Lerp(PopupText.localScale, Vector3.zero, 0.1f); }
+        else
+        {
+            if(ShowPopup)
+                PopupText.localScale = Vector3.Lerp(PopupText.localScale, Vector3.zero, 0.1f);
+            if(ShowOutline)
+                myOutline.enabled = false;
+
+        }
     }
 
     protected virtual void OnTriggerEnter(Collider col)
