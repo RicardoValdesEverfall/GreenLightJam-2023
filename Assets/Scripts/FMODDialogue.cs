f24b64050c255778c79207d2edaaf7e5107cfa42
ShaneGravestock <41488426+ShaneGravestock@users.noreply.github.com>
2023-06-28 19:57:34 +0100

    Added FMOD programmer instrument to play back lines of dialogue Added text to YarnSpinner

---
+++ Assets/Scripts/FMODDialogue.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using Yarn.Unity;
+using Yarn;
+
+public class FMODDialogue : MonoBehaviour
+{
+    FMOD.Studio.EVENT_CALLBACK dialogueCallback;
+
+    public FMODUnity.EventReference DialogueNarration;
+
+    
+
+#if UNITY_EDITOR
+    private void Reset()
+    {
+        DialogueNarration = FMODUnity.EventReference.Find("event:/DialogueNarration");
+    }
+#endif
+
+
+    private void Start()
+    {
+        dialogueCallback = new FMOD.Studio.EVENT_CALLBACK(DialogueEventCallback);
+    }
+
+
+    private void PlayDialogue(string lineID)
+    {
+        var dialogueInstance = FMODUnity.RuntimeManager.CreateInstance(DialogueNarration);
+
+        GCHandle stringHandle = GCHandle.Alloc(lineID);
+        dialogueInstance.setUserData(GCHandle.ToIntPtr(stringHandle));
+
+        dialogueInstance.setCallback(dialogueCallback);
+        dialogueInstance.start();
+        dialogueInstance.release();
+    }
+
+    [AOT.MonoPInvokeCallback(typeof(FMOD.Studio.EVENT_CALLBACK))]
+    static FMOD.RESULT DialogueEventCallback(FMOD.Studio.EVENT_CALLBACK_TYPE type, IntPtr instancePtr, IntPtr parameterPtr)
+    {
+        FMOD.Studio.EventInstance instance = new FMOD.Studio.EventInstance(instancePtr);
+
+        //Retrieve the user data
+        IntPtr stringPtr;
+        instance.getUserData(out stringPtr);
+
+        //Get the string object
+        GCHandle stringHandle = GCHandle.FromIntPtr(stringPtr);
+        string key = stringHandle.Target as String;
+
+        switch (type)
+        {
+            case FMOD.Studio.EVENT_CALLBACK_TYPE.CREATE_PROGRAMMER_SOUND:
+                {
+                    FMOD.MODE soundMode = FMOD.MODE.LOOP_NORMAL | FMOD.MODE.CREATECOMPRESSEDSAMPLE | FMOD.MODE.NONBLOCKING;
+                    var parameter = (FMOD.Studio.PROGRAMMER_SOUND_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.PROGRAMMER_SOUND_PROPERTIES));
+
+                    if (key.Contains("."))
+                    {
+                        FMOD.Sound dialogueSound;
+                        var soundResult = FMODUnity.RuntimeManager.CoreSystem.createSound(Application.streamingAssetsPath + "/" + key, soundMode, out dialogueSound);
+                        if (soundResult == FMOD.RESULT.OK)
+                        {
+                            parameter.sound = dialogueSound.handle;
+                            parameter.subsoundIndex = -1;
+                            Marshal.StructureToPtr(parameter, parameterPtr, false);
+                        }
+                    }
+                    else
+                    {
+                        FMOD.Studio.SOUND_INFO dialogueSoundInfo;
+                        var keyResult = FMODUnity.RuntimeManager.StudioSystem.getSoundInfo(key, out dialogueSoundInfo);
+                        if (keyResult != FMOD.RESULT.OK)
+                        {
+                            break;
+                        }
+
+                        FMOD.Sound dialogueSound;
+                        var soundResult = FMODUnity.RuntimeManager.CoreSystem.createSound(dialogueSoundInfo.name_or_data, soundMode | dialogueSoundInfo.mode, ref dialogueSoundInfo.exinfo, out dialogueSound);
+                        if (soundResult == FMOD.RESULT.OK)
+                        {
+                            parameter.sound = dialogueSound.handle;
+                            parameter.subsoundIndex = dialogueSoundInfo.subsoundindex;
+                            Marshal.StructureToPtr(parameter, parameterPtr, false);
+                        }
+                    }
+                    break;
+                }
+
+
+            case FMOD.Studio.EVENT_CALLBACK_TYPE.DESTROY_PROGRAMMER_SOUND:
+                {
+                    var parameter = (FMOD.Studio.PROGRAMMER_SOUND_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.PROGRAMMER_SOUND_PROPERTIES));
+                    var sound = new FMOD.Sound(parameter.sound);
+                    sound.release();
+
+                    break;
+                }
+
+
+            case FMOD.Studio.EVENT_CALLBACK_TYPE.DESTROYED:
+                {
+                    //Now the event has been destroyed, unpin the string memory so it can be garbage collected
+                    stringHandle.Free();
+
+                    break;
+                }
+        }
+        return FMOD.RESULT.OK;
+    }
+
+
+        void Update()
+        {
+
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                PlayDialogue("line:0b85daf");
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                PlayDialogue("line:02c62d9");
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                PlayDialogue("line:0f52539");
+            }
+
+        }
+
+
+}
