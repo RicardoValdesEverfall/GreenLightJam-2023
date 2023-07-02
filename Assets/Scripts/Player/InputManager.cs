c8731e81c1e57555d36e154f4067fc0ba52b611a
Ricardo Valdes <ricardovaldes@everfall-games.com>
2023-07-02 05:46:43 -0400

    Fixed shadergraph errors

--- Assets/Scripts/Player/InputManager.cs
+++ Assets/Scripts/Player/InputManager.cs
@@ -66,6 +66,7 @@ public class InputManager : MonoBehaviour
         HandleCameraInput();
         HandleSprintInput();
         HandleJumpInput();
+        HandleMeowInput();
         //HandleInteractInput
     }
 
